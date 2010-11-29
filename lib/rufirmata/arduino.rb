99987b5732121d9ef44cb0a7c94a6e9dfce0ce5f
PlasticLizard <hereiam@sonic.net>
2010-11-28 20:38:05 -0800

    Allow to prevent auto-starting of board listener thread so that it can be manually done from event machine.

--- lib/rufirmata/arduino.rb
+++ lib/rufirmata/arduino.rb
@@ -1,6 +1,6 @@
 class Arduino < Rufirmata::Board
   def initialize(serial_port_id, options={})
     super(serial_port_id, options.merge(:board_type=>:arduino))
-    start_listening
+    start_listening unless options[:auto_start] == false
   end
 end
--- lib/rufirmata/version.rb
+++ lib/rufirmata/version.rb
@@ -1,3 +1,3 @@
 module Rufirmata
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
